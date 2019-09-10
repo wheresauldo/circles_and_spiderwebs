1ee2cd4c2d6c5eb046ab02af9b9a6e2acf5619c0
wheresauldo <49892016+wheresauldo@users.noreply.github.com>
2019-09-09 23:31:28 -0400

    Add files via upload
    cleaner code, some variable name changes
    
    COMPLETED -  draw berry, draw guides 1&2. draw leaves
    Not working - draw seed outline
    Need to do - seed area included in guide1&2 that does not exceed outline

--- Strawberry.py
+++ Strawberry.py
@@ -12,8 +12,8 @@ def is_point_in_shape(x, y, shape):
 
 """
 
-def strawberry_top(berry_top):
-    turtle.setpos(top_of_berry)
+def strawberry_top():
+    turtle.setpos(straw_tri.get("top of berry"))
     turtle.color("green")
     for i in range(6):
         turtle.begin_fill()
@@ -22,7 +22,9 @@ def strawberry_top(berry_top):
         turtle.circle(50, 50)
         turtle.rt(70)
         turtle.end_fill()
-        
+
+my_strawberry = []
+
 def draw_berry(draw_here):
     turtle.setpos(draw_here)
     turtle.color("red")
@@ -36,8 +38,8 @@ def draw_berry(draw_here):
     turtle.goto(draw_here)
     turtle.end_fill()
     turtle.end_poly()
-    the_shape = turtle.get_poly()
-    return(the_shape)
+    my_strawberry.append.turtle.get_poly()
+    return(my_strawberry)
 
 def draw_seed(location):
         turtle.up()
@@ -54,87 +56,97 @@ def draw_seed(location):
         turtle.end_fill()
 
 def outline_point_seeds():
-    for location in the_shape:# to draw seeds on each pt on outline
-        if location < max(the_shape) and location > min(the_shape): 
+    for location in my_strawberry:# to draw seeds on each pt on outline
+        if location < max(my_strawberry) and location > min(my_strawberry):
             draw_seed(location)
-            
+
 
 # define highest x
 # define highest y
 # define lowest x
 # define lowest y
-  
 
-def draw_berry_dos():
+
+def draw_berry_dos(top_size, bottom_size):
     straw_tri = {}
-    
+
     draw_here = turtle.pos() # start pos
-    face_this_way = turtle.heading() 
+    face_this_way = turtle.heading()
 
     turtle.goto(draw_here) # "start"
     straw_tri.update({"start": draw_here})
     turtle.setheading(face_this_way)
-    
-    turtle.color("red") 
+
+    turtle.color("red")
     turtle.begin_poly() # create polygon
     turtle.begin_fill()
-    turtle.circle(50, 180)
-    
+    turtle.circle(top_size, 180)
+
     point_1 = turtle.pos() # "first point"
     straw_tri.update({"first point" : point_1})
-    turtle.lt(30) 
+    turtle.lt(30)
     turtle.forward(70)
-    
+
     point_2 = turtle.pos() # "second point"
     straw_tri.update({"second point" : point_2})
-    
-    turtle.circle(20, 100)
+
+    turtle.circle(bottom_size, 100)
+
 
     point_3 = turtle.pos() # "third point"
     straw_tri.update({"third point" : point_3})
     turtle.rt(30)
     turtle.goto(draw_here) # return to start
-    
+
     turtle.end_fill()
     turtle.end_poly() # close polygon
-    print(straw_tri)    
+    my_strawberry = turtle.get_poly()
+    return(straw_tri)
 
 straw_tri = {
-    'start': (0.00,0.00),
-    'first point': (0.00,100.00),
-    'second point': (-60.62,65.00),
-    'third point': (-65.94,34.82)
-        }
+    'start': (0.0, 0.0),
+    'second point': (-60.62, 65.0),
+    'top of berry': (53.03,53.03),
+    'strawberry tip': (-75.91,51.64),
+    'third point': (-65.94, 34.82),
+    'first point': (0.0, 100.0)
+    }
+
 def tri_guide():
     turtle.color("gold")
+    turtle.begin_poly()
     turtle.goto(straw_tri.get('start'))
     turtle.goto(straw_tri.get('first point'))
     print(turtle.heading())
     turtle.setheading(212.5)
     turtle.forward(90) # needs to be swapped out w/ variable
-    
+
 #update dict
     tip = turtle.pos() # add 'strawberry tip'
     straw_tri.update({"strawberry tip": tip})
-    
+
     turtle.goto(straw_tri.get('start')) # finish "bottom triangle"
-    
+    turtle.end_poly()
+    guide_one = turtle.get_poly()
+
+    turtle.begin_poly()
     turtle.setheading(45) # start "top triangle"
     turtle.forward(75) # needs height variable
-    
+
 #update dict
-    
+
     top = turtle.pos()
     straw_tri.update({'top of berry': top})
-    
+
     turtle.goto(straw_tri.get('first point')) # finish "top triangle"
-    
+    turtle.end_poly()
+    guide_two = turtle.get_poly()
 
 
-draw_berry_dos()
-
-
+draw_berry_dos(50, 20)
 tri_guide()
+print(straw_tri)
 
-
-
+outline_point_seeds()
+strawberry_top()
+raw_input('Press RETURN to exit')
